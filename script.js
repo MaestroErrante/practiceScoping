5a07776da62db8a5eac270c803a9b7b2a53dcd3b
Rudyard <rudyard90@gmail.com>
2021-02-22 09:59:17 -0600

    Create first function and global scope

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+//Global Variable
+const firstName = 'Rudy';
+calcAge(1990);
