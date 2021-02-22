1a62f244cb56a2115b809e40741bb2ce1a170621
Rudyard <rudyard90@gmail.com>
2021-02-22 10:07:01 -0600

    Code that is not accessible because of the scope chain

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ function calcAge(birthYear) {
 //Global Variable
 const firstName = 'Rudy';
 calcAge(1990);
+
+//Not able to access the age variable cause the scope chain
+
+console.log(age);
+
+//Same goes for functions
+
+printAge();
