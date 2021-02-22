20186debf01c34f3603fed3489f887adda18b45c
Rudyard <rudyard90@gmail.com>
2021-02-22 10:03:29 -0600

    Creat function within a function to create a second scope

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+
+  // Create a second scope
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
   return age;
 }
 
