b8de2ceb5aed8d19995e5e95ead2be11f23c0624
Rudyard <rudyard90@gmail.com>
2021-02-22 10:30:07 -0600

    create a function in block scope, return error because we are using strict mode

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function calcAge(birthYear) {
       var millenial = true;
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      // The scope of this add function is only in the block that is defined, making it a block scope because we are using strict mode
+      function add(a, b) {
+        return a + b;
+      }
     }
     //Not able to access, only inside the block that it was created
 
@@ -20,6 +25,9 @@ function calcAge(birthYear) {
 
     //Can be accessed, variable declared with var are function scopes, and they ignore the block scope
     console.log(millenial);
+
+    //Call it outside of the block, returns add is not defined
+    console.log(add(2, 3));
   }
 
   printAge();
